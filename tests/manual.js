9367a1c4a10182c118b1f7d6801755ea7e911d9b
DavidWells <hello@davidwells.io>
2019-04-14 16:51:41 -0700

    remove sig events on sync calls

--- tests/manual.js
+++ tests/manual.js
@@ -1,11 +1,19 @@
-const Configorama = require('../lib')
 const path = require('path')
+const Configorama = require('../lib')
 
 const yamlFile = path.join(__dirname, 'manualYaml.yml')
-const configorama = new Configorama(yamlFile)
 
-console.time('perf')
-configorama.init({}).then((result) => {
-  console.log(result)
-  console.timeEnd('perf')
-})
+/* async invoke */
+;(async () => {
+  console.time('asyncPerf')
+  const configorama = new Configorama(yamlFile)
+  const conf = await configorama.init({})
+  console.log('Async', conf)
+  console.timeEnd('asyncPerf')
+})()
+
+/* sync invoke */
+console.time('syncPerf')
+const confSync = Configorama.sync(yamlFile)
+console.log('confSync', confSync)
+console.timeEnd('syncPerf')
