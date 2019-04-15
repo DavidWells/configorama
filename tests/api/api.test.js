abcfd9d2afd18b2c5cd9a28e1d0bbf2d6cce6079
DavidWells <hello@davidwells.io>
2019-04-14 22:43:35 -0700

    add option to allow unknown variables to pass through

--- tests/api/api.test.js
+++ tests/api/api.test.js
@@ -23,3 +23,46 @@ test.cb('API is asynchronous', (t) => {
   })
   order.push('two')
 })
+
+test('Allow unknown variables to pass through', async (t) => {
+  const args = {
+    stage: 'dev',
+  }
+  const object = {
+    foo: 'bar',
+    env: '${consul:${self:region}}',
+    s3: '${s3:myBucket/myKey-${stage}}-hello',
+    ssm: '${ssm:/path/to/service/id}-service',
+    cloudformation: '${cf:another-stack.functionPrefix}-world',
+    custom: {
+      defaultStage: 'qa',
+      profiles: {
+        dev: 'devvvvvv',
+        qa: 'qaaaaaaa',
+        prod: 'prodddddd'
+      },
+      regions: {
+        dev: 'us-dev',
+        qa: 'us-qa',
+        prod: 'us-prod'
+      }
+    },
+    stage: '${opt:stage, self:custom.defaultStage}',
+    profile: '${self:custom.profiles.${self:stage}}',
+    region: '${opt:region, self:custom.regions.${self:stage}}',
+    key: '${opt:stage}',
+    whatever: '${stuff}'
+  }
+
+  const vars = new Configorama(object, {
+    passThrough: true
+  })
+
+  const config = await vars.init(args)
+  console.log(config)
+  t.is(config.env, '${consul:us-dev}')
+  t.is(config.s3, '${s3:myBucket/myKey-dev}-hello')
+  t.is(config.ssm, '${ssm:/path/to/service/id}-service')
+  t.is(config.cloudformation, '${cf:another-stack.functionPrefix}-world')
+  t.is(config.whatever, '${stuff}')
+})
