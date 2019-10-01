30197d632de343f1bd2ad5a40a7ee7939009199f
DavidWells <hello@davidwells.io>
2019-09-30 18:26:57 -0700

    feat: add allowUndefinedValues option
    default false. Throw if variable resolves undefined

--- tests/failCases/failCases.test.js
+++ tests/failCases/failCases.test.js
@@ -41,4 +41,30 @@ test('throw if opt not found', async (t) => {
   t.regex(error.message, /Variable not found/)
 })
 
+test('throw if value resolved is undefined', async (t) => {
+  const object = {
+    value: '${env:no, ${env:empty}}',
+  }
+
+  const error = await t.throwsAsync(async () => {
+    const x = await configorama(object, {
+      configDir: dirname
+    })
+    return x
+  })
+  t.regex(error.message, /Please verify the variable/)
+})
+
+test('Allow undefined values', async (t) => {
+  const object = {
+    value: '${env:no, ${env:empty}}',
+  }
+
+  const x = await configorama(object, {
+    configDir: dirname,
+    allowUndefinedValues: true
+  })
+  t.is(x.value, undefined)
+})
+
 // Nested fallbacks ACCESS_TOKEN = "${file(asyncValue.js, ${env:MY_SECxRET, 'hi'}, ${self:sharedValue})}"
