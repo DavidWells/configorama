5a65cc629a433e73f0383722ef3aed6dde67cf46
DavidWells <hello@davidwells.io>
2026-06-28 18:00:49 -0700

    docs: reorder guides sidebar

--- site/content/guides/_meta.js
+++ site/content/guides/_meta.js
@@ -1,13 +1,13 @@
 export default {
   'get-started': 'Get started',
-  'setup-wizard': 'Self configuring config',
   'first-config': 'Resolve your first config',
-  'file-references': 'Load files and secrets',
-  'custom-variable-sources': 'Write a custom variable source',
-  'bring-your-own-syntax': 'Bring your own syntax',
-  'dynamic-configuration': 'Build dynamic config',
-  'executable-configs': 'Use JS or TS config',
+  'setup-wizard': 'Self-configuring config',
   'inspect-config': 'Inspect config',
-  'use-in-ci': 'Use in CI',
-  'cloudformation-serverless': 'CloudFormation compatibility'
+  'dynamic-configuration': 'Build dynamic config',
+  'file-references': 'Load files and secrets',
+  'executable-configs': 'Use JS/TS config',
+  'bring-your-own-syntax': 'Bring your own syntax',
+  'custom-variable-sources': 'Custom variable source',
+  'cloudformation-serverless': 'CloudFormation compatibility',
+  'use-in-ci': 'Use in CI'
 }
