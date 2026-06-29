18860a5402ba9fbdce5d2089b8fb0d92971630ac
DavidWells <hello@davidwells.io>
2026-06-28 17:21:40 -0700

    docs: consolidate inspect guides

--- site/content/guides/_meta.js
+++ site/content/guides/_meta.js
@@ -7,10 +7,7 @@ export default {
   'custom-resolvers': 'Write a custom resolver',
   'dynamic-configuration': 'Build dynamic config',
   'executable-configs': 'Use JS or TS config',
-  'inspect-requirements': 'Inspect required inputs',
-  'safe-inspection': 'Safely inspect untrusted config',
-  'dependency-graphs': 'Generate dependency graphs',
-  'debug-resolution': 'Debug resolution',
+  'inspect-config': 'Inspect config',
   'use-in-ci': 'Use in CI',
-  'cloudformation-serverless': 'Use CloudFormation and Serverless'
+  'cloudformation-serverless': 'CloudFormation compatibility'
 }
