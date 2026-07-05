792535d7365acfb7eec32db54d53496fb67a2c58
DavidWells <hello@davidwells.io>
2026-07-04 17:15:48 -0700

    feat(onepassword): scaffold plugin package

---
+++ plugins/onepassword/README.md
@@ -0,0 +1,5 @@
+# configorama 1Password plugin
+
+Resolves configuration values from 1Password through the `op` CLI.
+
+Documentation forthcoming.
---
+++ plugins/onepassword/index.js
@@ -0,0 +1,8 @@
+/* 1Password variable source for configorama
+   Resolves ${op:alias} and ${op(item)} references through the op CLI */
+
+function createOnePasswordResolver(options = {}) {
+  throw new Error('Not implemented')
+}
+
+module.exports = createOnePasswordResolver
